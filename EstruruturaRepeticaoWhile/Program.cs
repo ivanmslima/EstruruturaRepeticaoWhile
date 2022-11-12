81a7424be9ba731f0a53cf4beaf0d39c687463a8
ivanmslima <81934424+ivanmslima@users.noreply.github.com>
2022-11-12 12:19:25 -0300

    Adicionar arquivos de projeto.

---
+++ EstruruturaRepeticaoWhile/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+
+namespace curso
+{
+    class Progam
+    {
+        static void Main(string[] args)
+        {
+            double idade, soma, media;
+            int cont;
+
+            Console.WriteLine("Digite números para calcular a média, e um número negativo para encerrar o programa. ");
+
+
+            idade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            soma = 0.0;
+            cont = 0;
+            while (idade >= 0)
+            {
+                soma = soma + idade;
+                cont = cont + 1;
+                idade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+            if (cont == 0)
+            {
+                Console.WriteLine("Impossível calcular");
+            }
+            else
+            {
+                media = soma / cont;
+                Console.WriteLine(media.ToString("F2", CultureInfo.InvariantCulture));
+            }
+            Console.WriteLine();
+
+
+
+        }
+    }
+}
